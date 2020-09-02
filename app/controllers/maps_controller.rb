c20426708df6e2e53d5cd3a6df83ee75886b958e
Harmandeep Mand <mr.mand@Harmandeeps-MacBook-Air.local>
2020-09-01 22:52:12 -0400

    Updates

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -8,11 +8,8 @@ class MapsController < ApplicationController
     def searchPlaces
         results = []
         status = ''
-        input = params[:input]+' in '+'['+params[:latitude]+','+params[:longitude]+']'
         search = GooglePlaces::Client.new('AIzaSyDMCLs_nBIfA8Bw9l50nSRwLOUByiDel9U')
-        byebug
-        search.spots_by_query(input).each do |place|
-            status = ''
+        search.spots_by_query(params[:input]).each do |place|
             if place.json_result_object['opening_hours']
                 status = place.json_result_object['opening_hours']['open_now']
             end
@@ -28,52 +25,6 @@ class MapsController < ApplicationController
             }
         end
         render json: results
-    end
-
-    def searchPlace
-        places = []
-        count = 0
-        distance = GoogleMapsService::Client.new(key: 'AIzaSyDMCLs_nBIfA8Bw9l50nSRwLOUByiDel9U')
-        search = GooglePlaces::Client.new('AIzaSyDMCLs_nBIfA8Bw9l50nSRwLOUByiDel9U')
-        search.spots(params[:lat],params[:lng], :name => params[:input], :radius => 10000).each do |place|
-            data = distance.distance_matrix([params[:lat],params[:lng]],
-                [place.json_result_object['geometry']['location']['lat'],place.json_result_object['geometry']['location']['lng']])
-            #places << {
-            #    'name' => place.json_result_object['name'],
-            #    'address' => places.json_result_object['formatted_address'],
-            #    'status' => place.json_result_object['opening_hours'],#['open_now']
-            #    'geo' => place.json_result_object['geometry']['location'],
-            #    'placeid' => place.json_result_object['place_id'],
-            #    'rating' => place.rating,
-            #    'types' => place.json_result_object['types'],
-            #    'distance' => data[:rows][0][:elements][0][:distance][:text],
-            #    'duration' => data[:rows][0][:elements][0][:duration][:text]
-            #}
-            places << place
-        end
-        search.spots_by_query(params[:input]).each do |place|
-            selected = false
-            places.each do|spot|
-                selected = true if spot.json_result_object['place_id'] == place.json_result_object['place_id']
-            end
-            if selected == false
-                data = distance.distance_matrix([params[:lat],params[:lng]],
-                    [place.json_result_object['geometry']['location']['lat'],place.json_result_object['geometry']['location']['lng']])
-                # places << {
-                #    'name' => place.json_result_object['name'],
-                #    'address' => places.json_result_object['formatted_address'],
-                #    'status' => place.json_result_object['opening_hours'],#['open_now']
-                #    'geo' => place.json_result_object['geometry']['location'],
-                #    'placeid' => place.json_result_object['place_id'],
-                #    'rating' => place.rating,
-                #    'types' => place.json_result_object['types'],
-                #    'distance' => data[:rows][0][:elements][0][:distance][:text],
-                #    'duration' => data[:rows][0][:elements][0][:duration][:text]
-                # }
-                places << place
-            end
-        end
-        render json: {'code' => 'Success', 'message' => places, 'result' => true}
     end
 
     def buildRoute
